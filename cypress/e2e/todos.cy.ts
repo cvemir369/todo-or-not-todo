80dcbeca8551fc2cfd6e960a1f64d250af91986e
cvemir369 <cvemir369@gmail.com>
2025-09-21 23:30:58 +0200

    cypress test todos

--- cypress/e2e/todos.cy.ts
+++ cypress/e2e/todos.cy.ts
@@ -15,4 +15,29 @@ describe("todo items", () => {
       .find("button")
       .should("have.attr", "title", "Mark as incomplete");
   });
+  it("should edit a todo item", () => {
+    cy.get("input[placeholder='Enter todo task']").type("New Todo Item{enter}");
+    cy.get("li").last().find("span").click().get("input").last().type("_edit");
+    cy.get('button:contains("Save")').click();
+    cy.get("li").last().should("contain.text", "New Todo Item_edit");
+  });
+  it("should delete a todo item", () => {
+    cy.get("input[placeholder='Enter todo task']").type("New Todo Item{enter}");
+    cy.get("li").last().find("span").click();
+    cy.get('button:contains("Delete")').last().click();
+    cy.get("li").last().should("not.contain.text", "New Todo Item");
+  });
+  it("should delete all todo items", () => {
+    cy.get("input[placeholder='Enter todo task']").type("New Todo Item{enter}");
+    cy.get("input[placeholder='Enter todo task']").type(
+      "Another Todo Item{enter}"
+    );
+    cy.get("button:contains('Delete All')").click();
+    cy.get("li").should("have.length", 0);
+  });
+  it("should load default todos", () => {
+    cy.get("button:contains('Delete All')").click();
+    cy.get("button:contains('Load Default Todos')").click();
+    cy.get("li").should("have.length.greaterThan", 0);
+  });
 });
